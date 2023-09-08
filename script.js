4a14913af64537fe4d189877add28c5b8522587b
bilalmukhtiar5 <billuedwardian08@gmail.com>
2023-09-08 15:23:48 +0500

    part3

--- script.js
+++ script.js
@@ -25,3 +25,25 @@ if(userGrade === "A"){
 }else if(userGrade === "F") {
     console.log("Sorry You are fail");
 }
+
+
+//part-3
+
+// program that checks if the number is positive, negative or zero
+// input from the user
+const number = parseInt(prompt("Enter a number: "));
+
+// check if number is greater than 0
+if (number > 0) {
+    console.log("The number is positive");
+}
+
+// check if number is 0
+else if (number == 0) {
+  console.log("The number is zero");
+}
+
+// if number is less than 0
+else {
+     console.log("The number is negative");
+}
